1d69f6dfaeae91fd631306627cb402a679a38c94
Pietro Lazzarotto <pietro@green-hippo.com>
2017-11-13 10:29:34 +0000

    Reached random website to check if it works.

---
+++ Scraping/TestCase.cs
@@ -0,0 +1,15 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Scraping
+{
+    class TestCase
+    {
+        static void Main(string[] args)
+        {
+            // Download: https://sites.google.com/a/chromium.org/chromedriver/downloads
+            IWebDriver driver = new ChromeDriver(@"C:\Program Files (x86)\ChromeDriver\");
+            driver.Url = "http://www.demoqa.com";
+        }
+    }
+}
